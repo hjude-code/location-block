bfa51d9bbab12bf4a33dc463ace248966f72c0e8
Harrison Jude <harrison@hjude.com>
2023-12-22 11:03:39 -0500

    updated borders and spacing to better integrate with theme

--- src/render.php
+++ src/render.php
@@ -8,21 +8,19 @@ $MapURL = "style=background-image:url('" . $attributes['MapURL'] . "')"
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<div class="LocationPannel" <?php echo $MapURL ?> >
-		<h4><?php echo($attributes['Address']) ?></h4>
-		<h3><?php echo($attributes['LocationName']) ?></h3>
+		<h4 class="wp-block-heading" ><?php echo($attributes['Address']) ?></h4>
+		<h3 class="wp-block-heading" ><?php echo($attributes['LocationName']) ?></h3>
 		<p><?php echo($attributes['YearStart']) ?></p>
 		<span class="rule"></span>
 		<p><?php echo($attributes['YearEnd']) ?></p>
 	</div>
 	<div class="LocationName">
-		<h3><?php echo($attributes['LocationName']) ?></h3>
+		<h3 class="wp-block-heading" ><?php echo($attributes['LocationName']) ?></h3>
 		<p><?php echo($attributes['Address']) ?></p>
 	</div>
-	<div class="Positions">
+	<div class="Positions wp-block-group">
 		<?php echo $content ?>
 		<div class="LocationMap" <?php echo $MapURL ?>></div>
 	</div>
-	<div class="Overview">
-		<?php echo($attributes['Overview']) ?>
-	</div>
+	<p class="Overview"> <?php echo($attributes['Overview']) ?> </p>
 </div>
